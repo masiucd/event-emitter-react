3ed0807cbd280dc0852760a63458e1bbd818f2c5
Masiu <ciszekmarcell@gmail.com>
2020-06-06 14:56:02 +0200

    add

--- gatsby-browser.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import { AppProvider } from "./src/context"
-import "./src/css/main.css"
-export const wrapRootElement = ({ element }) => {
-  return <AppProvider>{element}</AppProvider>
-}
--- src/components/Home/SmallBanner.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import styles from "../../css/smallBanner.module.css"
-const SmallBanner = ({ children }) => {
-  return <div className={styles.banner}>{children}</div>
-}
-
-export default SmallBanner
